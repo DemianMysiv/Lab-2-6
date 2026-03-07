5587cf513e08871ce309cfcb681192d8878bf686
Demyan <musiv31101979@gmail.com>
2026-03-07 20:21:11 +0200

    feat: complete modular structure and add gitignore

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "Employee.h"
+#include "Project.h"
+#include "Department.h"
+#include <windows.h> // Додаємо цю бібліотеку для роботи з консоллю Windows
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+
+    Employee emp1("MysivDemian");
+    Employee emp2("MysivDemyan", 797, 1000, "boss");
+
+    emp1.showProfile();
+    emp2.showProfile();
+
+    Department dep1;
+    Department dep2("buchalteria", 100);
+
+    dep1.hireEmployee(50);
+    dep2.hireEmployee(50);
+
+    Project prj1;
+
+    prj1.getStatusOfProject();
+
+    return 0;
+}
