0f14bbe397e98ec140556dd308bf4d1c49fe7c41
Demyan <musiv31101979@gmail.com>
2026-04-11 13:38:37 +0300

    Add entity creation functionality for admin

--- main.cpp
+++ main.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <memory>
 #include <windows.h>
+#include "IDisplayable.h"
+#include "Employee.h"
+#include "Project.h"
 
 void userMenu() {
     std::cout << "\n--- МЕНЮ КОРИСТУВАЧА ---\n";
 }
 
-void adminMenu() {
-    std::cout << "\n--- МЕНЮ АДМІНІСТРАТОРА ---\n";
+void adminMenu(std::vector<std::unique_ptr<IDisplayable>>& data) {
+    int choice;
+    do {
+        std::cout << "\n1. ДОДАТИ працівника\n2. ДОДАТИ проєкт\n0. Вихід\nДія: ";
+        std::cin >> choice;
+
+        if (choice == 1) {
+            std::string name;
+            std::cout << "Ім'я: "; 
+            std::cin >> name;
+            data.push_back(std::make_unique<Employee>(name));
+            std::cout << "Працівника додано!\n";
+        } else if (choice == 2) {
+            std::string title; 
+            int d;
+            std::cout << "Назва: "; 
+            std::cin >> title;
+            std::cout << "Тижні: "; 
+            std::cin >> d;
+            data.push_back(std::make_unique<Project>(title, d));
+            std::cout << "Проєкт додано!\n";
+        }
+    } while(choice != 0);
 }
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
+    std::vector<std::unique_ptr<IDisplayable>> companyData;
     int role;
     const std::string ADMIN_PASS = "1234";
 
@@ -32,7 +59,7 @@ int main() {
                 std::cin >> inputPass;
                 
                 if (inputPass == ADMIN_PASS) {
-                    adminMenu(); 
+                    adminMenu(companyData); 
                 } else {
                     std::cout << "Відмова в доступі! Невірний пароль!\n";
                 }
